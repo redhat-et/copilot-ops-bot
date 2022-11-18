085a5e2649f0063269d20e261290d43f19844e34
Oleg <97077423+RobotSail@users.noreply.github.com>
2022-11-18 10:48:18 -0500

    fix commenting issue
    Signed-off-by: Oleg <97077423+RobotSail@users.noreply.github.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,49 +1,56 @@
-import { Context } from 'probot';
-import { Counter } from 'prom-client';
-import { getTokenSecretName } from '@operate-first/probot-kubernetes';
-import parseIssueForm from '@operate-first/probot-issue-form';
+import { Context } from "probot";
+import { Counter } from "prom-client";
+import { getTokenSecretName } from "@operate-first/probot-kubernetes";
+import parseIssueForm from "@operate-first/probot-issue-form";
 import {
   LABEL_COPILOT_OPS_BOT,
   REROLL_COMMAND,
   COPILOT_OPS_TASK,
-} from './constants';
+  COPILOT_OPS_BOT,
+  COPILOT_OPS_BOT_DEV,
+} from "./constants";
 
-import { Issue } from '@octokit/webhooks-types/schema';
+import { Issue, Repository } from "@octokit/webhooks-types/schema";
 
 // Simple callback wrapper - executes and async operation and based on the result it inc() operationsTriggered counted
 const wrapOperationWithMetrics = async (
   promise: Promise<any>,
   labels: any,
-  counter: Counter<string>
+  counter: Counter<string>,
 ) => {
-  labels.operation = 'k8s';
+  labels.operation = "k8s";
   try {
     await promise;
-    labels.status = 'Succeeded';
+    labels.status = "Succeeded";
   } catch (err) {
-    labels.status = 'Failed';
-    console.error('Error', err);
+    labels.status = "Failed";
+    console.error("Error", err);
     throw err;
   } finally {
     counter.labels(labels).inc();
   }
 };
 
+/** Extract user input from the Probot context.
+ *
+ * @param context Probot context object.
+ * @returns issue along with the provided user's input.
+ */
 export const parseIssueInfo = async (context: Context) => {
   try {
     const form = await parseIssueForm(context);
     if (!form.botInput) return;
     const issue = context.issue();
-    console.log('issue.opened', issue);
+    console.log("issue.opened", issue);
     return {
       ...issue,
       userInput:
-        typeof form.botInput === 'string'
+        typeof form.botInput === "string"
           ? form.botInput
-          : form.botInput.join('\n'),
+          : form.botInput.join("\n"),
     };
   } catch (err) {
-    console.log('error parsing form: ', err);
+    console.log("error parsing form: ", err);
     return;
   }
 };
@@ -60,22 +67,22 @@ const generateTaskRunPayload = (
   name: string,
   context: any,
   userInput: string,
-  issueNum?: string
+  issueNum?: string,
 ) => {
   const issueNumber = issueNum || context.issue().issue_number;
   let finalIssueNum: string;
-  if (typeof issueNumber !== 'string') {
-    if (typeof issueNumber === 'number') {
+  if (typeof issueNumber !== "string") {
+    if (typeof issueNumber === "number") {
       finalIssueNum = issueNumber.toString();
     } else {
-      throw 'issueNumber is not a valid type';
+      throw "issueNumber is not a valid type";
     }
   } else {
     finalIssueNum = issueNumber;
   }
   return {
-    apiVersion: 'tekton.dev/v1beta1',
-    kind: 'TaskRun',
+    apiVersion: "tekton.dev/v1beta1",
+    kind: "TaskRun",
     metadata: {
       // "copilot-ops-bot" to match the prefix in manifests/base/tasks/kustomization.yaml namePrefix
       // (not necessary for functionality, just for consistency)
@@ -90,27 +97,27 @@ const generateTaskRunPayload = (
       },
       params: [
         {
-          name: 'REPO_NAME',
+          name: "REPO_NAME",
           value: context.issue().repo,
         },
         {
-          name: 'ISSUE_NUMBER',
+          name: "ISSUE_NUMBER",
           value: finalIssueNum,
         },
         {
-          name: 'ISSUE_OWNER',
+          name: "ISSUE_OWNER",
           value: context.issue().owner,
         },
         {
-          name: 'SECRET_NAME',
+          name: "SECRET_NAME",
           value: getTokenSecretName(context),
         },
         {
-          name: 'USER_INPUT',
+          name: "USER_INPUT",
           value: userInput,
         },
         {
-          name: 'PR_FLAG',
+          name: "PR_FLAG",
           value: LABEL_COPILOT_OPS_BOT,
         },
       ],
@@ -133,7 +140,7 @@ export const getBranchName = (issueNumber: number) => {
  */
 export const rerollUserInput = (body: string): string => {
   // remove command
-  const newBody = body.replace(REROLL_COMMAND, '').trim();
+  const newBody = body.replace(REROLL_COMMAND, "").trim();
   return newBody;
 };
 
@@ -153,7 +160,7 @@ export const isReroll = (body: string): boolean => {
  * @return Corresponding issue number.
  */
 export const getIssueNumberFromPR = (body: string): number => {
-  const issueNumRegexp = new RegExp(/Fixes #(\d+)/, 'g');
+  const issueNumRegexp = new RegExp(/Fixes #(\d+)/, "g");
   const matches = issueNumRegexp.exec(body);
   if (matches === null) {
     return -1;
@@ -171,8 +178,8 @@ export const getIssueNumberFromPR = (body: string): number => {
  */
 const stripPrompt = (issueBody: string): string => {
   const promptOffset = 2;
-  const asLines = issueBody.split('\n').splice(promptOffset);
-  return asLines.join('\n');
+  const asLines = issueBody.split("\n").splice(promptOffset);
+  return asLines.join("\n");
 };
 
 export const getOriginalUserInput = async (
@@ -180,7 +187,7 @@ export const getOriginalUserInput = async (
   issueNumber: number,
   context: Context,
   owner: string,
-  repoName: string
+  repoName: string,
 ) => {
   const { octokit } = context;
   let userInput: string;
@@ -192,8 +199,8 @@ export const getOriginalUserInput = async (
       repo: repoName,
     });
     const { body } = originalIssue.data;
-    if (typeof body === 'undefined' || body === null) {
-      userInput = '';
+    if (typeof body === "undefined" || body === null) {
+      userInput = "";
     } else {
       userInput = stripPrompt(body);
       console.log(`parsed input for reroll: "${userInput}"`);
@@ -202,14 +209,14 @@ export const getOriginalUserInput = async (
   // parse the user input from the original issue
   else {
     const issueInfo = await parseIssueInfo(context);
-    userInput = issueInfo?.userInput || '';
+    userInput = issueInfo?.userInput || "";
   }
   return userInput;
 };
 
 export const getIssueNumber = (issue: Issue) => {
   let issueNumber: number;
-  if (typeof issue.pull_request !== 'undefined') {
+  if (typeof issue.pull_request !== "undefined") {
     if (issue.body === null) {
       return;
     }
@@ -223,14 +230,14 @@ export const getIssueNumber = (issue: Issue) => {
 export const isCopilotOpsPR = (issue: Issue) => {
   const labels = issue.labels || [];
   const existingLabel = labels.find((v) => v.name === LABEL_COPILOT_OPS_BOT);
-  return typeof existingLabel === 'undefined';
+  return typeof existingLabel !== "undefined";
 };
 
 export const addBotLabel = async (
   context: Context,
   issueNumber: number,
   owner: string,
-  repo: string
+  repo: string,
 ) => {
   const { octokit } = context;
   await octokit.issues
@@ -240,7 +247,46 @@ export const addBotLabel = async (
       repo,
       labels: [LABEL_COPILOT_OPS_BOT],
     })
-    .catch((e) => console.error('could not apply label:', e));
+    .catch((e) => console.error("could not apply label:", e));
+};
+
+/**
+ *
+ * @param context Context object for the issue_comment.created webhook event
+ * @param issue GitHub Issue object
+ * @param repo Repository object
+ * @returns Whether the given issue is a copilot ops bot issue.
+ */
+export const isOurIssue = async (
+  context: Context,
+  issue: Issue,
+  repo: Repository,
+): Promise<boolean> => {
+  const { octokit } = context;
+  if (typeof issue.pull_request !== "undefined") {
+    const ourPR = isCopilotOpsPR(issue);
+    return ourPR;
+  }
+
+  // is this is a related thread?
+  const data = await octokit.reactions
+    .listForIssue({
+      issue_number: issue.number,
+      owner: repo.owner.login,
+      repo: repo.name,
+    })
+    .then((d) => d.data);
+  if (typeof data === "undefined") {
+    return false;
+  }
+  // select the bot
+  const botConfirm = data.filter(
+    (v) =>
+      v.content === "eyes" &&
+      (v.user?.login === COPILOT_OPS_BOT ||
+        v.user?.login === COPILOT_OPS_BOT_DEV),
+  );
+  return botConfirm.length === 0;
 };
 
 export { wrapOperationWithMetrics, generateTaskRunPayload };
